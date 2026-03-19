af67e9e893b9142f97d505bf5bcac6000fe9e5be
joy9163 <joy9163@joy9163deMacBook-Air-5.local>
2026-03-19 19:01:05 +0800

    Revert config for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // appDir is now the default in Next.js 13+, no need for experimental flag
-  output: 'export',
-  images: {
-    unoptimized: true
-  }
 }
 
 module.exports = nextConfig
